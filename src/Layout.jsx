49c1baa1b4d316c8e4eeb6a0ab7d6fa31e8873e4
Carolina Mejia <caromejia088@gmail.com>
2026-03-05 19:17:48 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { createPageUrl } from '@/utils';
 
 // Portal pages handle their own layout (sidebar + header).
 // Chart page is full-screen.
